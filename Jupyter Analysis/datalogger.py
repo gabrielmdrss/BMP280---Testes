4730a39807930329112986916828cbb6a31aded5
gabrielmdrss <gabriel.mdrs.04@gmail.com>
2024-10-28 21:41:56 -0300

    Translation of .csv and .ipynb files

--- Jupyter Analysis/datalogger.py
+++ Jupyter Analysis/datalogger.py
@@ -2,21 +2,26 @@ from datetime import datetime
 import serial
 
 baud = 1000000
-porta = 'COM3'
-arquivo = "comparacaoDoisSensoresLadoaLado.csv"
-cabeçalho = "Data, Hora,Pressao Atmosferica 1,Temperatura 1,Pressao Atmosferica 2,Temperatura 2\n"
+port = 'COM3'
+file = "data.csv"
+header = "Date, Time,Atmospheric pressure,Temperature\n"
 
-ser = serial.Serial(porta, baud)
+'''
+file = "comparisonTwoSensors.csv"
+header = "Date,Time,Atmospheric pressure 1,Temperature 1,Atmospheric pressure 2,Temperature 2\n"
+'''
+
+ser = serial.Serial(port, baud)
 ser.flushInput()
-file = open(arquivo, "a")
-file.write(cabeçalho)
+file = open(file, "a")
+file.write(header)
 
 while True:
 
-    data_hora_atual = datetime.now()
-    hora_formatada = data_hora_atual.strftime("%H:%M, ")
-    data_formatada = data_hora_atual.strftime("%Y/%m/%d, ")
-    dados = data_formatada + hora_formatada + str(ser.readline().decode("utf-8"))
-    file = open(arquivo, "a")
-    file.write(dados)
+    current_date_time = datetime.now()
+    formatted_time = current_date_time.strftime("%H:%M, ")
+    formatted_date = current_date_time.strftime("%Y/%m/%d, ")
+    data = formatted_date + formatted_time + str(ser.readline().decode("utf-8"))
+    file = open(file, "a")
+    file.write(data)
     
